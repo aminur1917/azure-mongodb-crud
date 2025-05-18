834eada76409f8c25efd64f6eb147ad38aa48b2f
mrahman032 <mrahman032@citymail.cuny.edu>
2025-05-18 14:18:53 -0400

    Sppring crud dockerfile added

---
+++ mongodb/src/main/java/com/mongodb/model/Task.java
@@ -0,0 +1,61 @@
+package com.mongodb.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "tasks")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Task {
+
+    @Id
+    private String taskId;
+    private String description;
+    private int severity;
+    private String assignee;
+    private int storyPoint;
+
+    public String getTaskId() {
+        return taskId;
+    }
+
+    public void setTaskId(String taskId) {
+        this.taskId = taskId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getSeverity() {
+        return severity;
+    }
+
+    public void setSeverity(int severity) {
+        this.severity = severity;
+    }
+
+    public String getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(String assignee) {
+        this.assignee = assignee;
+    }
+
+    public int getStoryPoint() {
+        return storyPoint;
+    }
+
+    public void setStoryPoint(int storyPoint) {
+        this.storyPoint = storyPoint;
+    }
+}
